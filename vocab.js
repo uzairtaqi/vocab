e5e28b26b52eb44c7f8a219a81a74d71878ed45e
uzairtaqi <48158450+uzairtaqi@users.noreply.github.com>
2019-03-07 18:19:51 -0500

    Add files via upload

--- vocab.js
+++ vocab.js
@@ -1,5 +1,327 @@
-var oJson = {
-    "اللَّهُ":"God",
+var Vocab1Nouns = 
+{
+"اللَّهُ":"God",
+"إِلَهٌ - آلِهَةٌ":"Deity (That which is worshipped & obeyed)",
+"اَجَرٌ - اُجُورٌ":"Reward",
+"آيَةٌ - آيَاتٌ":"Sign/Verse",
+"بَلَدٌ - بِلَدٌ":"Town",
+"رَسُولٌ - رُسُلٌ":"Messenger",
+"يَوم - اَيَّامٌ":"Day",
+"آمِين":"Faithful",
+"آخَرٌ":"Last",
+"رَحْمَةٌ":"Love & Care",
+"عَذَابٌ":"Punishment",
+"فَوزٌ":"Victory",
+"قُرْآنٌ":"Recital",
+"كِتَابٌ - كُتُبٌ":"Book, Document, Law",
+"خَلِيفَة":"Successors",
+"اَرْضٌ":"Earth",
+"اُمٌّ - اُمَّهَاتٌ":"Mother",
+"شَدِيدٌ - شِدَادٌ":"Strong, Severe",
+"عَظِيمٌ":"Mighty",
+"قَوِّيٌ":"Strong",
+"كَبِيرٌ":"Big",
+"كَرِيمٌ":"Noble, Generous",
+"مُجِيدٌ":"Glorious",
+"وَاسِعٌ":"Wide, ample",
+"مُبِينٌ":"Clear & Clarifying",
+"اَوَّلُ - اَوَّلُونَ":"First",
+"اُولى":"First (f)",
+"آخَرُ -  آَخَرُونَ":"Other",
+"اُخْرَى - اُخَرُ":"Other (f)"
+}
+
+
+var Vocab1Verbs =
+{
+    "نَصَرَ  -  ينْصُرُ ":" to help",
+    "بَلَغَ  -  يَبْلُغُ ":" to reach",
+    "تَرَكَ  -  يَتْرُكُ ":" to leave or to let be",
+    "حَشَرَ  -  يَحْشُرُ/ يَحْشِرُ ":" to gather",
+    "حَكَمَ  -  يَحْكُمُ ":" to pass judgement",
+    "خَرَجَ  -  يَخْرُجُ ":" to go out",
+    "خَلَدَ  -  يَخْلُدُ ":" to remain or last forever",
+    "خَلَقَ  -  يَخْلُقُ ":" to create",
+    "دَخَلَ  -  يَدْخُلُ ":" to enter",
+    "ذَكَرَ  -  يَذْكُرُ ":" to remember"
+}
+
+var Vocab2Nouns = {
+    "رَجُلٌ - رِجَالٌ":"Man",
+    "سَمَاءٌ - سَمَاوَاتٌ":"Sky",
+    "عَبْدٌ - عِبَادٌ":"Servant, Slave",
+    "قَرْنٌ - قُرُونٌ":"Generation",
+    "قَومٌ - اَقْوَامٌ":"People",
+    "مَنْفَعَةٌ - مَنافِعٌ":"Benifit",
+    "كَافِرٌ - كَافِرُونَ":"Unbeliever, ungrateful",
+    "مُؤْمِنٌ - مُؤْمِنُونَ":"Believer",
+    "ظَالِمٌ - ظَالِمُونَ":"Wrongdoer, Oppressor",
+    "اَعْجَمِيٌّ - اَعْجَمِيُّونَ":"Foreign",
+    "عَرَبِيٌّ - عَرَبٌ":"Arab, Arabic",
+    "مُسْلِمٌ - مُسْلِمُونَ":"Muslim",
+    "نَصْرَانِيٌّ - نَصَارَى":"Christian",
+    "يَهُودِيٌّ - يَهُودٌ":"Jew",
+    "سَنَةٌ - سِنُونَ":"Year",
+    "فَاكِهَةٌ - فَوَاكِهُ":"Fruit",
+    "كَثِيْرٌ - كَثِيرُونَ":"Many",
+    "كِثَارٌ":"Many (pl male)",
+    "تَوَّابٌ - تَوَّابُونَ":"Relenting",
+    "مٌكْرَمٌ - مُكْرَمُونَ":"Honoured",
+    "دَرَجَةٌ - دَرَجَاتٌ":"Station"
+}
+
+var Vocab2Verbs = {
+    "فَعَلَ  -  يَفْعَلُ ":" to act or to do",
+    "فَتَحَ  -  يَفْتَحُ ":" to open",
+    "بَعَثَ  -  يَبْعَثُ ":" to send",
+    "جَعَلَ  -  يَجْعَلُ ":" to make",
+    "جَمَعَ  -  يَجْمَعُ ":" to gather",
+    "ذَهَبَ  -  يَذْهَبُ ":" to go",
+    "رَفَعَ  -  يَرْفَعُ ":" to raise or lift",
+    "سَحَرَ  -  يَسْحَرُ ":" to bewitch or charm or enchant",
+    "صَلَحَ  -  يَصْلَحُ ":" to be good or right",
+    "لَعَنَ  -  يَلْعَنُ ":" to curse",
+    "نَفَعَ  -  يَنْفَعُ ":" to be useful or benefit"    
+}
+
+var Vocab3Nouns = {
+    "جَنَّةٌ - جَنَّاتٌ":"Garden",
+    "جَارِيَةٌ - جَوَارٌ":"Ship, Young woman",
+    "حَقٌّ - حُقُوقٌ":"Truth, True",
+    "فِتْنَةٌ - فِتَنٌ":"Affliction, Temptation",
+    "بُشْرَ":"Good tidings",
+    "الدُنْيَا":"World",
+    "وَادٍ":"Valley",
+    "عَصًا":"Stick, Staff",
+    "نَذِيْرٌ - نُذُرٌ":"Warner",
+    "هُدًى":"Guidance",
+    "مَكَّة":"Makkah",
+    "فَقِيْرٌ - فُقَرَاءُ":"Poor",
+    "أَسْوَدُ - سُودٌ":"Black",
+    "أَبْيَضٌ - بِيضْ":"White",
+    "بَيْضَاءُ":"White (f)",
+    "عَالٍ - عَالِيَةٌ":"High, Lofty",
+    "لَطِيفٌ":"Gentle",
+    "جَاهِلٌ - جَاهِلُونَ":"Ignorant",
+    "حَاكِمٌ - حَاكِمُونَ":"Authority",
+    "تَأدِيبٌ":"Comportment"
+}
+
+var Vocab3Verbs = {
+"ضَرَبَ  -  يَضْرِبُ ":" to beat or strike",
+"حَمَلَ  -  يَحْمِلُ ":" to carry or bear",
+"صَبَرَ  -  يَصْبِرُ ":" to bind or to be patient or to be forbearing",
+"ظَلَمَ  -  يَظْلِمُ ":" to do wrong or evil or oppress",
+"عَرَفَ  -  يَعْرِفُ ":" to know",
+"عَقَلَ  -  يَعْقِلُ ":" to hobble with the 'iqal, to intern, to confine",
+"غَفَرَ  -  يَغْفِرُ ":" to forgive",
+"قَدَرَ  -  يَقْدِرُ ":" to decree, ordain or decide",
+"كَذَبَ  -  يَكْذِبُ ":" to lie or to deceive",
+"كَسَبَ  -  يَكْسِبُ ":" to gain, acquire, earn, win",
+"مَلَكَ  -  يَمْلِكُ ":" to own, to take over, to lay hold",
+}
+
+var Vocab4Nouns = {
+    "نَارٌ - نِيرَانٌ":"Hell",
+    "صَدَقَةٌ - صَدَقَاتٌ":"Alms",
+    "عُرْفٌ - أَعْرَافٌ":"Crest, Ridge, Height",
+    "سَبِيلٌ - سُبُلٌ":"Way, Road, Path ",
+    "سَفَرٌ - أَسْفَارٌ":"Journey",
+    "ضَلَالٌ":"Error",
+    "فَسَادٌ":"Corruption",
+    "حَمْدٌ":"Praise",
+    "أَهْلٌ - أَهْلُونَ":"People, Family, Folk",
+    "شَمْسٌ":"Sun (f)",
+    "صَابِرٌ":"Patient, Steadfast",
+    "مِنْ":"From, Of",
+    "فِي":"In, Concerning",
+    "لِ":"To, For",
+    "بِ":"In, At, By",
+    "عَلَى":"Upon",
+    "اِلَى":"To",
+    "مَعَ":"With",
+    "بَينَ":"Between, Among",
+    "جَاهِلٌ - جَاهِلُونَ":"Ignorant"
+}
+
+var Vocab4Verbs = {
+"سَمِعَ  - يَسْمَعُ ":" to hear",
+"حَزِنَ  -  يَحْزَنُ ":" to be sad or grieved",
+"حَسِبَ  -  يَحْسَبُ ":" to regard or consider",
+"حَفِظَ  -  يَحْفِظُ ":" to preserve, protect, guard",
+"خَسِرَ  -  يَخْسَرُ ":" to incur a loss, suffer damage, to lose",
+"رَحِمَ  -  يَرْحَمُ ":" to have compassion",
+"شَهِدَ - يَشْهَدُ ":" to witness",
+"عَمِلَ  -  يَعْمَلُ ":" to do or act",
+"كَرِهَ  -  يَكْرَهُ ":" to feel disgust, be disgusted"
+}
+
+
+var Vocab5Nouns = {
+    "اِبْنٌ - أَبْنَاءٌ - بَنُونٌ":"Son",
+    "وَلِيٌّ - أَوْلِيَاءُ":"Friend, Protector",
+    "خَالِدٌ - خَالِدُونَ":"Remaining in, everlasting ",
+    "صَالِحٌ":"Righteous",
+    "نِعْمَةٌ":"Blessing, Bounty",
+    "أَخٌ - اِخْوَانٌ - اِخْوَةٌ":"Brother",
+    "رَبٌّ - أَرْبَابٌ":"Lord",
+    "عَمَلٌ - أَعْمَالٌ":"Work",
+    "حَسْبٌ":"Sufficiency, Reckoning",
+    "قَرِيبٌ":"Near",
+    "سَمِيعٌ":"Hearing",
+    "حَيَاةٌ":"Life"
+}
+
+var Vocab5Verbs = {
+    "حّيَّ ":" to live",
+"رَدَّ ":" to send back",
+"صَدَّ ":" to turn away",
+"ضَرَّ ":" to harm",
+"ضَلَّ ":" to lose one's way or go astray",
+"ظَنَّ ":" to think, believe or assume",
+"عَدَّ ":" to count or number",
+"غَرَّ ":" to mislead, deceive",
+"مَدَّ ":" to extend, distend, expand",
+"مَسَّ ":" to feel",
+"وَدَّ ":" to love, like or be fond"
+}
+
+
+var Vocab6Nouns = {
+    "الرَّحْمَانُ":"The Merciful",
+    "العَالِمِين":"Created Beings",
+    "شَفًا":"Lip, Rim, Edge",
+    "خُفْرَةٌ":"Pit",
+    "أَبٌ - آبَاءٌ":"Father",
+    "صَاحِبٌ - أَصْحَابٌ":"Companion, Inhabitant",
+    "طَائِفَةٌ":"Group, Number, Party",
+    "اِمْرَأَةٌ - نِسَاءٌ - نِسْوَةٌ":"Woman",
+    "أَمْرٌ - أُمُورٌ":"Affair, Matter, Command",
+    "إِنْسَانٌ - اَنَّاسُ":"Man, People (pl)",
+    "ذُو - أُولُو":"Possessor of",
+    "اِسْمٌ - اَسْمَاءٌ":"Name",
+    "أَلْبَابٌ":"Hearts, Intellects",
+    "بَصَرٌ - أَبْصَارٌ":"Sight, Eye",
+    "يَدٌ - أَيدٍ":"Hand (f)",
+    "عَدُوٌّ - أَعْدَاءٌ":"Enemy",
+    "شَيْطَانٌ - شَيَاطِينُ":"Devil",
+    "سَوْءٌ":"Evil",
+    "عَاقِبَةٌ":"End, Consequence",
+    "مَدِينَةٌ - مُدُنٌ":"City",
+    "بَيْتٌ - بُيُوتٌ":"House"
+}
+
+var Vocab6Verbs = {
+    "وَذَرَ - يَزَرُ":"To leave",
+"وَضَعَ - يَضَعُ":"To lay",
+"وَقَعَ - يَقَعُ":"To fall",
+"وَهَبَ - يَهَبُ":"To give, donate",
+"وَجَدَ - يَجِدُ":"To find",
+"وَرِثَ - يَرِثُ":"To inherit",
+"وَزَرَ - يَزِرُ":"To take upon or to carry",
+"وَصَفَ - يَصِفُ":"To describe",
+"وَعَدَ - يَعِدُ":"To promise",
+"وَقَى - يَقِى":"To guard or preserve",
+"وَسُعَ - وَسِعَ - يَوْسُعُ - يَسَعُ":"To be wide, vast or extensive"
+
+}
+
+
+var Vocab7Nouns = {
+    "أُمَّةٌ - أُمَمٌ":"Community",
+    "إِبْنَةٌ - بَنَاتٌ":"Daughter",
+    "شَيْءٌ - أَشْيَاءُ":"Thing",
+    "دَارٌ - دُورٌ  - دِيَارٌ":"Abode (f)",
+    "حِزْبٌ - أَحْزَابٌ":"Party",
+    "أَثَرٌ - آثَارٌ":"Track; effect",
+    "لِسَانٌ - أَلْسِنَةٌ":"Tongue",
+    "نَاقَةٌ - إِبِلٌ":"She Camel",
+    "نَهْرٌ - أَنْهَارٌ":"River",
+    "فَضْلٌ":"Bounty",
+    "سِحْرٌ":"Magic",
+    "صِرِاطٌ":"Way, Path",
+    "قَمِيصٌ":"Shirt",
+    "مُجْرِمٌ":"Sinner",
+    "قَلِيلٌ":"Few",
+    "مُسْتَقِيم":"Straight ",
+    "عَجِيبٌ":"Amazing, Strange, Wonderful",
+    "حَلَالٌ":"Permissible",
+    "حَرَامٌ":"Forbidden, Sacred",
+    "بَعِيدٌ":"Far reaching, distant",
+    "يَسِيْرٌ":"Easy",
+    "عَسِيْرٌ":"Difficult",
+    "قَبْلَ":"Before",
+    "بَعْدَ":"After"    
+}
+
+var Vocab7Verbs = {
+    "تَابَ - يَتُوبُ":"To repent",
+"ذَاقَ - يَذُوقُ":"To taste, sample or try",
+"فَازَ - يَفُوزُ":"to be successful",
+"قَالَ - يَقُولُ":"to speak, say, tell",
+"قَامَ - يَقُومُ":"to stand or get up",
+"كَان - يَكُونُ":"to be or exist",
+"مَاتَ - يَمُوتُ":"to die or perish",
+"خَاف - يَخَافُ":"to be afraid",
+"كَادَ - يَكَادُ":"to be on point",
+"كَادَ - يَكِيدُ":"to deceive",
+"زَادَ - يَزِيدُ":"to become greater",
+"بَاعَ - يَبِيعُ":"to sell"
+
+}
+
+
+var Vocab8Nouns = {
+    "سَيِّئَةٌ - سَيِّئَاتٌ":"Evil, Ill",
+    "قَمَرٌ":"Moon",
+    "ضِيَاءٌ":"Light, Illumination",
+    "نُورٌ - أَنْوَارٌ":"Light",
+    "مَثَلٌ - أَمْثَالٌ":"Likeness, Parallel",
+    "مَطْلِعٌ":"Rising-place",
+    "مَغْرِبٌ - مَغَارِبٌ":"Setting-place",
+    "بَرٌّ":"Land",
+    "بَحْرٌ - بِحَارٌ":"Sea",
+    "حَمْلٌ":"Load",
+    "يَوْمُ الْبَعْثِ":"Day of Resurrection",
+    "زَبُورٌ":"The Psalms",
+    "خَفِيفٌ":"Light (adj.)"    
+}
+
+var Vocab8Verbs = {
+    "تَلا - يَتْلُوْ":"To follow, succeed",
+"دَعَا - يَدْعُوْ":"To call",
+"عَفَا - يَعْفُوْ":"To be or become obliterated",
+"بَغَى - يَبْغِى":"To see or to desire",
+"جَرَى - يَجْرِى":"To flow, to run, to hurry",
+"جَزَى - يَجْزِى":"To requite or recompense",
+"قَضَى - يَقْضِى":"To settle or to finish",
+"كَفَى - يَكْفِى":"To be sufficient",
+"هَدَى - يَهْدِى":"To guide",
+"خَشِيَ - يَخْشَى":"To fear",
+"نَسِيَ - يَنْسَى":"To forget"
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var AllVocabNouns = {
+"اللَّهُ":"God",
 "إِلَهٌ - آلِهَةٌ":"Deity (That which is worshipped & obeyed)",
 "اَجَرٌ - اُجُورٌ":"Reward",
 "آيَةٌ - آيَاتٌ":"Sign/Verse",
@@ -121,13 +443,51 @@ var oJson = {
 "سَوْءٌ":"Evil",
 "عَاقِبَةٌ":"End, Consequence",
 "مَدِينَةٌ - مُدُنٌ":"City",
-"بَيْتٌ - بُيُوتٌ":"House"
+"بَيْتٌ - بُيُوتٌ":"House",
+"أُمَّةٌ - أُمَمٌ":"Community",
+"إِبْنَةٌ - بَنَاتٌ":"Daughter",
+"شَيْءٌ - أَشْيَاءُ":"Thing",
+"دَارٌ - دُورٌ  - دِيَارٌ":"Abode (f)",
+"حِزْبٌ - أَحْزَابٌ":"Party",
+"أَثَرٌ - آثَارٌ":"Track; effect",
+"لِسَانٌ - أَلْسِنَةٌ":"Tongue",
+"نَاقَةٌ - إِبِلٌ":"She Camel",
+"نَهْرٌ - أَنْهَارٌ":"River",
+"فَضْلٌ":"Bounty",
+"سِحْرٌ":"Magic",
+"صِرِاطٌ":"Way, Path",
+"قَمِيصٌ":"Shirt",
+"مُجْرِمٌ":"Sinner",
+"قَلِيلٌ":"Few",
+"مُسْتَقِيم":"Straight ",
+"عَجِيبٌ":"Amazing, Strange, Wonderful",
+"حَلَالٌ":"Permissible",
+"حَرَامٌ":"Forbidden, Sacred",
+"بَعِيدٌ":"Far reaching, distant",
+"يَسِيْرٌ":"Easy",
+"عَسِيْرٌ":"Difficult",
+"قَبْلَ":"Before",
+"بَعْدَ":"After",
+"سَيِّئَةٌ - سَيِّئَاتٌ":"Evil, Ill",
+    "قَمَرٌ":"Moon",
+    "ضِيَاءٌ":"Light, Illumination",
+    "نُورٌ - أَنْوَارٌ":"Light",
+    "مَثَلٌ - أَمْثَالٌ":"Likeness, Parallel",
+    "مَطْلِعٌ":"Rising-place",
+    "مَغْرِبٌ - مَغَارِبٌ":"Setting-place",
+    "بَرٌّ":"Land",
+    "بَحْرٌ - بِحَارٌ":"Sea",
+    "حَمْلٌ":"Load",
+    "يَوْمُ الْبَعْثِ":"Day of Resurrection",
+    "زَبُورٌ":"The Psalms",
+    "خَفِيفٌ":"Light (adj.)" 
+
  }
 
- 
- var verb = 
+
+ var AllVocabVerbs = 
  {
-     "نَصَرَ  -  ينْصُرُ ":" to help",
+"نَصَرَ  -  ينْصُرُ ":" to help",
 "بَلَغَ  -  يَبْلُغُ ":" to reach",
 "تَرَكَ  -  يَتْرُكُ ":" to leave or to let be",
 "حَشَرَ  -  يَحْشُرُ/ يَحْشِرُ ":" to gather",
@@ -178,20 +538,47 @@ var oJson = {
 "غَرَّ ":" to mislead, deceive",
 "مَدَّ ":" to extend, distend, expand",
 "مَسَّ ":" to feel",
-"وَدَّ ":" to love, like or be fond"
-     
+"وَدَّ ":" to love, like or be fond",
+"تَلا - يَتْلُوْ":"To follow, succeed",
+"دَعَا - يَدْعُوْ":"To call",
+"عَفَا - يَعْفُوْ":"To be or become obliterated",
+"بَغَى - يَبْغِى":"To see or to desire",
+"جَرَى - يَجْرِى":"To flow, to run, to hurry",
+"جَزَى - يَجْزِى":"To requite or recompense",
+"قَضَى - يَقْضِى":"To settle or to finish",
+"كَفَى - يَكْفِى":"To be sufficient",
+"هَدَى - يَهْدِى":"To guide",
+"خَشِيَ - يَخْشَى":"To fear",
+"نَسِيَ - يَنْسَى":"To forget",
+"تَابَ - يَتُوبُ":"To repent",
+"ذَاقَ - يَذُوقُ":"To taste, sample or try",
+"فَازَ - يَفُوزُ":"to be successful",
+"قَالَ - يَقُولُ":"to speak, say, tell",
+"قَامَ - يَقُومُ":"to stand or get up",
+"كَان - يَكُونُ":"to be or exist",
+"مَاتَ - يَمُوتُ":"to die or perish",
+"خَاف - يَخَافُ":"to be afraid",
+"كَادَ - يَكَادُ":"to be on point",
+"كَادَ - يَكِيدُ":"to deceive",
+"زَادَ - يَزِيدُ":"to become greater",
+"بَاعَ - يَبِيعُ":"to sell",
+"وَذَرَ - يَزَرُ":"To leave",
+"وَضَعَ - يَضَعُ":"To lay",
+"وَقَعَ - يَقَعُ":"To fall",
+"وَهَبَ - يَهَبُ":"To give, donate",
+"وَجَدَ - يَجِدُ":"To find",
+"وَرِثَ - يَرِثُ":"To inherit",
+"وَزَرَ - يَزِرُ":"To take upon or to carry",
+"وَصَفَ - يَصِفُ":"To describe",
+"وَعَدَ - يَعِدُ":"To promise",
+"وَقَى - يَقِى":"To guard or preserve",
+"وَسُعَ - وَسِعَ - يَوْسُعُ - يَسَعُ":"To be wide, vast or extensive"
+
  }
 
  var tableRef;
- var pdf = new jsPDF('l', 'pt', [1550.28,  1100.89])
 
 
- function downloadPDF()
- {
-    pdf.autoTable({html: "#Nouns", tableWidth: number = 300, startY: 120, tableLineColor: [0, 0, 0],
-        tableLineWidth: 1, headStyles: {fillColor: [255,255,255], textColor:[237,23,29]}});
-    
- }
 function generateList(tableName, jsonFile, bodyName)
 {
     tableRef = document.getElementById(tableName).getElementsByTagName('tbody')[0];
@@ -212,9 +599,20 @@ function generateList(tableName, jsonFile, bodyName)
     var htext2  = document.createTextNode('Arabic'); //arabic
     Head1.appendChild(htext2) //english
     Head2.appendChild(htext1) //arabic
+    var amt;
+    const testamt = 20;
 
+    if (jsonFile === AllVocabNouns || jsonFile === AllVocabVerbs)
+    {
+        amt = testamt;
+    }
+    else
+    {
+     amt = result.length;
+   
+    }
 
-    for (var i=0;i<20;i++)
+    for (var i=0;i<amt;i++)
     {
         var newRow   = tableRef.insertRow(tableRef.rows.length);
         var Cell1  = newRow.insertCell(0);
@@ -225,103 +623,80 @@ function generateList(tableName, jsonFile, bodyName)
 
         if (getRandomInt(2) == 0)
         {
-            Cell1.appendChild(newText2) //arabic
+            Cell1.appendChild(newText2); //arabic
+            Cell2.appendChild(newText); //english -added
+          //  Cell2.style.color="transparent";
+            Cell2.style.visibility="hidden";
 
+           
+           
         }
         else 
         {
-			Cell2.appendChild(newText); //english
+            Cell2.appendChild(newText); //english
+            Cell1.appendChild(newText2); //arabic -added
+           // Cell1.style.color="transparent";
+            Cell1.style.visibility="hidden";
+
         }
-    }
+    }    
+
 
 }
 
-function generateLists(){
+function generateLists(NounJson, VerbJson){
    
-generateList('Nouns', oJson, '#NounsB');
-generateList('Verbs', verb, '#VerbsB');
+generateList('Nouns', NounJson, '#NounsB');
+generateList('Verbs', VerbJson, '#VerbsB');
 
 }
 
-function emptyTable()
-{
-    $("#Nouns").empty();
-    $("#Verbs").empty();
-}
 
 $(document).ready(function(){
-    $('#Refresh').click(function(){
-       generateLists();
+    $('#All').click(function(){
+       generateLists(AllVocabNouns, AllVocabVerbs);
     });
-    $('#Download').click(function(){
-downloadPDF();
+    $('#V1').click(function(){
+        generateLists(Vocab1Nouns, Vocab1Verbs);
+     });
+     $('#V2').click(function(){
+        generateLists(Vocab2Nouns, Vocab2Verbs);
+     });
+     $('#V3').click(function(){
+        generateLists(Vocab3Nouns, Vocab3Verbs);
+     });
+     $('#V4').click(function(){
+        generateLists(Vocab4Nouns, Vocab4Verbs);
+     });
+     $('#V5').click(function(){
+        generateLists(Vocab5Nouns, Vocab5Verbs);
+     });
+     $('#V6').click(function(){
+        generateLists(Vocab6Nouns, Vocab6Verbs);
+     });
+     $('#V7').click(function(){
+        generateLists(Vocab7Nouns, Vocab7Verbs);
+     });
+     $('#V8').click(function(){
+        generateLists(Vocab8Nouns, Vocab8Verbs);
+     });
+    
+     $('#Answers').click(function(){
+         getAnswers();
      });
   });
 
-/*
 
-(function() {
-    
-var tableRef = document.getElementById('Nouns').getElementsByTagName('tbody')[0];
-var tableRef2 = document.getElementById('Verbs').getElementsByTagName('tbody')[0];
 
-    var result = [];
-    for(var i in oJson)
-    {
-        result.push([i, oJson[i]]);
-    }    
-    result = shuffle(result);
-
-	
-	
-    var result2 = [];
-    for(var k in verb)
-    {
-        result2.push([k, verb[k]]);
-    }    
-    result2 = shuffle(result2);
-	
-	 for (var p=0;p<20;p++)
-    {
-        var newRow2   = tableRef2.insertRow(tableRef2.rows.length);
-        var Cell3  = newRow2.insertCell(0);
-        var Cell4  = newRow2.insertCell(1);
-        var newText3 = document.createTextNode(result2[p][1]); //english
-        var newText4  = document.createTextNode(result2[p][0]); //arabic
- 
-        if (getRandomInt(2) == 0)
-        {
-            Cell3.appendChild(newText4) //arabic
-        }
-        else 
-        {
-			Cell4.appendChild(newText3); //english
-        }
+  function getAnswers(){
+    var x = document.getElementsByTagName('td');
+   for(i=0;i<x.length;i++) {
+      // x[i].style.color ="black";
+      x[i].style.visibility ="visible";
     }
-	
-	
-    for (var i=0;i<20;i++)
-    {
-        var newRow   = tableRef.insertRow(tableRef.rows.length);
-        var Cell1  = newRow.insertCell(0);
-        var Cell2  = newRow.insertCell(1);
-        var newText  = document.createTextNode(result[i][1]); //english
-        var newText2  = document.createTextNode(result[i][0]); //arabic
- 
-		
-        if (getRandomInt(2) == 0)
-        {
-            Cell1.appendChild(newText2) //arabic
-        }
-        else 
-        {
-			Cell2.appendChild(newText); //english
-        }
-    }
-})();
-*/
+}
 
-
+  
 
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
